fd05892dcd863deb4d98804dd7326d7d697e76a9
Direktor379 <rozumenko379@gmail.com>
2025-04-21 17:18:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -381,13 +381,13 @@ async def monitor_cluster_trades():
 
                     signal = None
                     if total_buy >= 1000:
-                    signal = "BOOSTED_LONG"
+                       signal = "BOOSTED_LONG"
                     elif total_sell >= 1000:
-                    signal = "BOOSTED_SHORT"
+                        signal = "BOOSTED_SHORT"
 
-# 📊 Логуємо потужні кластери, які не стали BOOSTED
-                    if signal is None and (total_buy > 300 or total_sell > 300):
-                    send_message(f"📊 Кластер {strongest_bucket[0]} → Buy: {round(total_buy)}, Sell: {round(total_sell)} | Не BOOSTED (поріг: 1000)")
+                   # 📊 Логуємо потужні кластери, які не стали BOOSTED
+                   if signal is None and (total_buy > 300 or total_sell > 300):
+                      send_message(f"📊 Кластер {strongest_bucket[0]} → Buy: {round(total_buy)}, Sell: {round(total_sell)} | Не BOOSTED (поріг: 1000)")
 
                         signal = "BOOSTED_SHORT"
 
