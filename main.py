1a41b262d8f00da3bf8dfcb532be61215bb9d755
Direktor379 <rozumenko379@gmail.com>
2025-04-24 09:07:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -673,11 +673,11 @@ async def monitor_trailing_stops():
 
                     if new_sl:
                        if trailing_stops[side] == new_sl:
-                           continue  # ⛔️ Пропускаємо — такий стоп уже стоїть
+                          continue  # ⛔️ Пропускаємо — такий стоп уже стоїть
 
-                            trailing_stops[side] = new_sl  # ✅ Оновлюємо тільки, якщо справді новий
+                       trailing_stops[side] = new_sl  # ✅ Оновлюємо тільки, якщо справді новий
 
-                            # send_message(f"🔁 {side}: Новий трейлінг-стоп {new_sl} (+{profit_pct:.2f}%)")
+                         # send_message(f"🔁 {side}: Новий трейлінг-стоп {new_sl} (+{profit_pct:.2f}%)")
                             cancel_existing_stop_order(side)
                             ...
 
